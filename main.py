b0ba30d5fc11019e9840e62a6f981941ca71bac6
havelockz <paradox.vr@gmail.com>
2023-04-24 10:38:06 +0300

    update

--- main.py
+++ main.py
@@ -38,10 +38,10 @@ updater = Updater(bot_token, use_context=True)
 
 # Додаємо обробники повідомлень та команд
 updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, message_handler))
-updater.dispatcher.add_handler(CommandHandler('s', reply_handler))
-updater.start_webhook(listen='0.0.0.0', port=int(os.environ.get('PORT', 5000)), url_path='6280626788:AAFLql4KSb-r9fHTKxcOmSnR0yG5PzFarfI')
-updater.bot.set_webhook('https://answerbot111.herokuapp.com//6280626788:AAFLql4KSb-r9fHTKxcOmSnR0yG5PzFarfI')
-updater.idle()
-# Запускаємо бота
-#updater.start_polling()
+#updater.dispatcher.add_handler(CommandHandler('s', reply_handler))
+#updater.start_webhook(listen='0.0.0.0', port=int(os.environ.get('PORT', 5000)), url_path='6280626788:AAFLql4KSb-r9fHTKxcOmSnR0yG5PzFarfI')
+#updater.bot.set_webhook('https://answerbot111.herokuapp.com//6280626788:AAFLql4KSb-r9fHTKxcOmSnR0yG5PzFarfI')
 #updater.idle()
+# Запускаємо бота
+updater.start_polling()
+updater.idle()
